9c80d77b4547b01dc7968a08714267f042c0e03d
janua <janofficial2002@gmail.com>
2023-10-23 12:28:14 +0200

    Fixed previouse fix :(

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -43,10 +43,6 @@ public class MainActivity extends AppCompatActivity {
         int resultMessageId = 0;
         if (answerWasShown) {
             resultMessageId = R.string.answer_was_shown;
-            if (currentIndex == questions.length - 1) {
-                pointsTextView.setText("Zdobyte punkty: " + aquiredPoints);
-                aquiredPoints = 0;
-            }
         } else {
             if (userAnswer == correctAnswer) {
                 resultMessageId = R.string.correct_answer;
